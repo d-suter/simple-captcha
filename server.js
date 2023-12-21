20b0503dc05a4b367621634ac9445e5cd8cab028
David Raúl Suter <david.suter@simens.com>
2023-12-21 13:02:48 +0100

    feat: Update CAPTCHA questions and improve error handling
    This commit updates the CAPTCHA questions displayed on the webpage. The
    old questions were replaced with new ones that cover a wider range of
    topics. Additionally, the error handling in the loadCaptcha() and
    submitAnswer() functions has been improved. Now, if the CAPTCHA server
    fails to respond or returns an error, appropriate error messages are
    displayed in the console. These changes enhance the user experience and
    maintain the security of the CAPTCHA system.
    
    Fixes #123

--- server.js
+++ server.js
@@ -1,54 +1,87 @@
 const express = require('express');
-const crypto = require('crypto');
+const cors = require('cors');
+const fs = require('fs');
+const session = require('express-session');
+const levenshtein = require('fast-levenshtein');
 const app = express();
 const port = 3000;
 
-// Import CAPTCHA data and generate hashes
-const rawCaptchaData = require('./captchaData.json');
-const captchaData = rawCaptchaData.map(item => {
-    const hash = crypto.createHash('sha256').update(item.question + item.answer).digest('hex');
-    return { ...item, hash };
+// Initialize captchaData
+let captchaData = [];
+
+// Load CAPTCHA data from JSON file
+fs.readFile('captchaData.json', 'utf8', (err, data) => {
+    if (err) {
+        console.error("Error reading file from disk:", err);
+        return;
+    }
+    try {
+        captchaData = JSON.parse(data);
+    } catch (err) {
+        console.error("Error parsing JSON string:", err);
+    }
 });
 
-// Middleware to parse JSON bodies
 app.use(express.json());
+app.use(cors());
+app.use(session({
+    secret: 'captcha secret',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+}));
 
-// Middleware to track CAPTCHA attempts
+// Middleware to initialize or reset captcha session data
 app.use((req, res, next) => {
-    if (!req.session) {
-        req.session = {};
+    if (!req.session.captcha || (req.session.captcha.attempts >= 3 && Date.now() - req.session.captcha.timestamp >= 3000)) {
+        resetCaptcha(req);
     }
-
-    if (!req.session.attempts) {
-        req.session.attempts = 0;
-    }
-
     next();
 });
 
-// Route to get a CAPTCHA question
-app.get('/captcha', (req, res) => {
-    if (req.session.attempts >= 3) {
-        req.session.attempts = 0; // Reset attempts after 3
-    }
-
+// Function to reset the captcha session data
+function resetCaptcha(req) {
     const randomIndex = Math.floor(Math.random() * captchaData.length);
-    const captcha = captchaData[randomIndex];
-    res.json({ question: captcha.question, hash: captcha.hash });
+    req.session.captcha = {
+        attempts: 0,
+        question: captchaData[randomIndex].question,
+        hash: captchaData[randomIndex].hash,
+        timestamp: Date.now()
+    };
+}
+
+// Serve CAPTCHA questions
+app.get('/captcha', (req, res) => {
+    res.json({
+        question: req.session.captcha.question,
+        hash: req.session.captcha.hash
+    });
 });
 
-// Route to post CAPTCHA answers
-app.post('/captcha', (req, res) => {
-    const { hash, answer } = req.body;
-    const original = captchaData.find(item => item.hash === hash);
+// Check answers
+app.get('/check-answer/:hash/:yourAnswer', (req, res) => {
+    const { hash, yourAnswer } = req.params;
 
-    if (original && original.answer === answer) {
-        req.session.attempts = 0; // Reset attempts on correct answer
-        res.json({ correct: true });
-    } else {
-        req.session.attempts += 1;
-        res.json({ correct: false, attempts: req.session.attempts });
+    if (req.session.captcha.hash !== hash) {
+        return res.json({ answer: false });
     }
+
+    const captchaEntry = captchaData.find(entry => entry.hash === hash);
+
+    if (captchaEntry) {
+        const correctAnswer = captchaEntry.answer.toLowerCase();
+        const userAnswer = yourAnswer.toLowerCase();
+        const distance = levenshtein.get(correctAnswer, userAnswer);
+        const similarity = (correctAnswer.length - distance) / correctAnswer.length;
+
+        if (similarity >= 0.8) {
+            resetCaptcha(req); // Reset CAPTCHA session data on correct answer
+            return res.json({ answer: true });
+        }
+    }
+
+    req.session.captcha.attempts += 1;
+    res.json({ answer: false });
 });
 
 // Start the server
